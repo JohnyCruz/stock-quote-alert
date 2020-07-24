5b5c7f36e6b9202d4234bc6045fd9baaf4d40c2e
Johny Cruz <johnycruz00@gmail.com>
2020-07-24 20:17:49 -0300

    Cria validação se usuário passou 3 parametros de entrada

---
+++ ClassLibrary_stock-quote-alert/Validacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_stock_quote_alert
+{
+    public class Validacao
+    {
+        public static bool ValidaParametrosEntrada(string[] args)
+        {
+            if (args.Length != 3) return false;
+            else return true;
+        }
+    }
+}
--- UnitTestProject-stock-quote-alert/UnitTest1.cs
+++ UnitTestProject-stock-quote-alert/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary_stock_quote_alert;
 
 namespace UnitTestProject_stock_quote_alert
 {
@@ -7,8 +8,16 @@ namespace UnitTestProject_stock_quote_alert
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestaEntradaDeParametros()
         {
+            string[] args = new string[3] { "PETR4", "22.67", "22.59" };
+            Assert.IsFalse(Validacao.ValidaParametrosEntrada(new string[1] {"PETR4"}),
+                           "Usuário passou apenas 1 parâmetro de entrada");
+            Assert.IsFalse(Validacao.ValidaParametrosEntrada(new string[2] { "PETR4", "22.67"}),
+                           "Usuário passou apenas 2 parâmetros de entrada");
+            Assert.IsTrue(Validacao.ValidaParametrosEntrada(new string[3] { "PETR4", "22.67", "22.59" }),
+                           "Usuário passou 3 parâmetros de entrada");
+
         }
     }
 }
