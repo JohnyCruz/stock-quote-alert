1ddaca243e4ee5b765303e511cd3daea40460bad
Johny Cruz <johnycruz00@gmail.com>
2020-07-26 16:15:45 -0300

    Instalacao MetroMordernUI

--- WindowsForms_stock-quote-alert/FormMain.cs
+++ WindowsForms_stock-quote-alert/FormMain.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace WindowsForms_stock_quote_alert
 {
-    public partial class Form1 : Form
+    public partial class FormMain : MetroFramework.Forms.MetroForm
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
