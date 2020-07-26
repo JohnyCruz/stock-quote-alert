dfd55ddd6cf63c3b8cf0144454a5708c3c7137a6
Johny Cruz <johnycruz00@gmail.com>
2020-07-26 17:09:28 -0300

    Garantir configuracoes no App.config
    Coloquei uma atualizacao para garantir que o app config terá as informações padrão

--- WindowsForms_stock-quote-alert/FormMain.cs
+++ WindowsForms_stock-quote-alert/FormMain.cs
@@ -15,10 +15,51 @@ namespace WindowsForms_stock_quote_alert
     {
         private static AbortableBackgroundWorker backgroundWorker1 = new AbortableBackgroundWorker();
 
-
+        Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
         public FormMain()
         {
             InitializeComponent();
+            AtualizaCamposConfiguracao();
+            
+        }
+        private void AtualizaCamposConfiguracao()
+        {
+            try
+            {
+                metroTextBoxEmailPara.Text = config.AppSettings.Settings["EmailTo"].Value;
+                metroTextBoxEmailDe.Text = config.AppSettings.Settings["EmailFrom"].Value;
+                metroTextBoxCredencialEmail.Text = config.AppSettings.Settings["CredentialEmail"].Value;
+                metroTextBoxCredencialSenha.Text = config.AppSettings.Settings["CredentialPassword"].Value;
+                metroTextBoxSMTPHost.Text = config.AppSettings.Settings["SMTPHost"].Value;
+                metroTextBoxSMTPPort.Text = config.AppSettings.Settings["SMTPPort"].Value;
+                metroTextBoxAPIKey.Text = config.AppSettings.Settings["APIKey"].Value;
+            }
+            catch (Exception)
+            {
+                //Aqui estou adicionando configurações padrões para o app
+
+                config.AppSettings.Settings.Add("EmailTo", "");
+                config.AppSettings.Settings.Add("EmailFrom", "");
+                config.AppSettings.Settings.Add("CredentialEmail", "");
+                config.AppSettings.Settings.Add("CredentialPassword", "");
+                config.AppSettings.Settings.Add("SMTPHost", "");
+                config.AppSettings.Settings.Add("SMTPPort", "");
+                config.AppSettings.Settings.Add("APIKey", "");
+                config.AppSettings.Settings.Add("Culture", "");
+                config.Save(ConfigurationSaveMode.Modified);
+                config.AppSettings.Settings["EmailTo"].Value = "johnycruz00@gmail.com";
+                config.AppSettings.Settings["EmailFrom"].Value = "JohnyInoa@gmail.com";
+                config.AppSettings.Settings["CredentialEmail"].Value = "JohnyInoa@gmail.com";
+                config.AppSettings.Settings["CredentialPassword"].Value = "teste#1aoni";
+                config.AppSettings.Settings["SMTPHost"].Value = "smtp.gmail.com";
+                config.AppSettings.Settings["SMTPPort"].Value = "587";
+                config.AppSettings.Settings["APIKey"].Value = "9368a2aa";
+                config.AppSettings.Settings["Culture"].Value = "en-US";
+
+                config.Save(ConfigurationSaveMode.Modified);
+                AtualizaCamposConfiguracao();
+            }
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
